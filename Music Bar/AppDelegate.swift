e67e0c434dda15acc14f4bb3612b3cf65b034561
musa11971 <mussesemou99@gmail.com>
2019-11-24 17:01:15 +0100

    Initial Commit

---
+++ Music Bar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Music Bar
+//
+//  Created by Musa Semou on 24/11/2019.
+//  Copyright © 2019 Musa Semou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Music Bar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Music Bar
+//
+//  Created by Musa Semou on 24/11/2019.
+//  Copyright © 2019 Musa Semou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
